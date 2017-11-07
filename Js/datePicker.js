b901c28f4ce28a4a15ceca1ab48379f1bb792cbf
YHL <yhl@mail.hamastar.com.tw>
2017-11-07 13:32:49 +0800

    新增目錄

---
+++ Js/datePicker.js
@@ -0,0 +1,39 @@
+define(function(){
+	
+	function main(env, opt, file){
+
+		var $set = {
+				language: 'en',
+				todayButton: new Date(),
+				debug: false
+			}
+
+		$.extend($set, opt);
+
+		// var $elem = document.createElement('input'); //建立一個虛擬 input
+		// $elem.setAttribute('type', 'date'); //指定為日期欄位
+
+		// if ( $elem.type === 'text' ) { //如果 type 仍是 text，及代表尚未實作 date picker
+
+			var $this = $(env),
+				$date = $this.find('[type="date"]');
+
+			$date.each(function(i,n){
+				var $this = $(this),
+					_val = $this.attr('value');
+
+				$this.attr('type', 'text');
+				$this.attr('value', _val);
+
+				$(n).datepicker($set);
+			});
+		// }
+
+		if($set.debug) {
+			console.log('預設值:', $set);
+			console.log('檔案 '+ file +'.js 已順利執行。');
+		}
+	}
+	
+	return main;
+});
---
+++ Js/linkType.js
@@ -0,0 +1,69 @@
+define(function(){
+	
+	function main(env, opt, file){
+
+		var $set = {
+				externalClass: 'link', //外部連結的 class
+				domains: [],
+				debug: false
+			}
+
+		$.extend($set, opt);
+
+		var $this = $(env),
+			$a = $this.find('a').filter(function(i){ //沒有圖片的 a
+				return !($(this).find('*').length)
+			});
+
+		var $domains = $set.domains;
+
+		$domains.push(window.location.hostname);
+
+		var $domains_l = $domains.length;
+
+		$a.each(function(i, d){
+			var $this = $(this),
+				_href = $this.attr('href') || '',
+				_dot_i = _href.lastIndexOf('.'),
+				_slash = _href.lastIndexOf('/');
+
+			var _has_http = !!(_href.match(/^https?\:\/\//)),
+				_is_local = -1; //如果是本地或指定檔案
+
+			if( _href === '#' || _href.indexOf('javascript:') === 0 ) {
+				return true;
+			}
+
+			for( var i = 0; i < $domains_l; i++) {
+				_is_local = Math.max(_is_local, _href.indexOf($domains[i])); //只要有一個大於-1，就是內部
+			}
+
+			if( _has_http && _is_local === -1 ) { //如果有 http(s) 又不是本地 就是外部連結
+				$this.addClass($set.externalClass); //加入外部連結 class
+
+			}else if( _dot_i > _slash ) { //最後是 .*
+				var _dot_i = _href.lastIndexOf('.') + 1, //最後一個.
+					_ques_i =_href.indexOf('?'), // 最後一個問候
+					_length = _href.length,
+					_type = 'other';
+
+				if( _ques_i === -1 || _dot_i > _ques_i ){ //如果沒有問號或問號比.前面
+					_length = _href.length - _dot_i;
+				}else { //如果有問號
+					_length = _ques_i - _dot_i;
+				}
+
+				_type = _href.substr(_dot_i, _length);
+
+				$this.addClass(_type);
+			}
+		});
+
+		if($set.debug) {
+			console.log('預設值:', $set);
+			console.log('檔案 '+ file +'.js 已順利執行。');
+		}
+	}
+	
+	return main;
+});
